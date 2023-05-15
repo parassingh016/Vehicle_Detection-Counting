8793b7cf667494f62ed6456528a9c588fbcc81a0
Paras Singh <56398287+parassingh016@users.noreply.github.com>
2023-05-15 11:29:34 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,84 +1,81 @@
-import cv2
+import  cv2
 import numpy as np
- 
-min_contour_width = 40  
-min_contour_height = 40  
-offset = 10  
-line_height = 550  
-matches = []
-cars = 0
- 
- 
-def get_centrolid(x, y, w, h):
-   x1 = int(w / 2)
-   y1 = int(h / 2)
- 
-   cx = x + x1
-   cy = y + y1
-   return cx, cy
- 
- 
- 
-#cap = cv2.VideoCapture('C:/Users/paras/Downloads/02_detection.mp4')#
-cap = cv2.VideoCapture(0) #depends on your system 0, -1 or 1
- 
- 
-cap.set(3, 1920)
-cap.set(4, 1080)
- 
-if cap.isOpened():
-   ret, frame1 = cap.read()
-else:
-   ret = False
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
- 
- 
-while ret:
-   d = cv2.absdiff(frame1, frame2)
-   grey = cv2.cvtColor(d, cv2.COLOR_BGR2GRAY)
- 
-   blur = cv2.GaussianBlur(grey, (5, 5), 0)
- 
-   ret, th = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-   dilated = cv2.dilate(th, np.ones((3, 3)))
-   kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
- 
-  
-   closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
-   contours, h = cv2.findContours(
-       closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-   for(i, c) in enumerate(contours):
-       (x, y, w, h) = cv2.boundingRect(c)
-       contour_valid = (w >= min_contour_width) and (
-           h >= min_contour_height)
- 
-       if not contour_valid:
-           continue
-       cv2.rectangle(frame1, (x-10, y-10), (x+w+10, y+h+10), (255, 0, 0), 2)
- 
-       cv2.line(frame1, (0, line_height), (1200, line_height), (0, 255, 0), 2)
-       centrolid = get_centrolid(x, y, w, h)
-       matches.append(centrolid)
-       cv2.circle(frame1, centrolid, 5, (0, 255, 0), -1)
-       cx, cy = get_centrolid(x, y, w, h)
-       for (x, y) in matches:
-           if y < (line_height+offset) and y > (line_height-offset):
-               cars = cars+1
-               matches.remove((x, y))
-               # print(cars)
- 
-   cv2.putText(frame1, "Total Cars Detected: " + str(cars), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1,
-               (0, 170, 0), 2)
- 
- 
- 
-   cv2.imshow("Vehicle Detection", frame1)
-   #cv2.imshow("Difference" , th)
-   if cv2.waitKey(1) == 27:
-       break
-   frame1 = frame2
-   ret, frame2 = cap.read()
- 
+
+#web Camera
+cap = cv2.VideoCapture('C:/Users/paras/Downloads/VeDetCoun-main/VeDetCoun-main/Vehiclevdo.mp4')
+
+min_width_rect = 80  #min width rectangle
+min_hieght_rect = 80  #min hieght rectangle
+
+count_line_position = 550
+
+# initialize Substructor
+algo = cv2.createBackgroundSubtractorMOG2()
+#algo = cv2.bgsegm.createBackgroundSubtractorMOG2()
+
+def center_handle(x,y,w,h):
+    x1=int(w/2)
+    y1=int(h/2) 
+    cx= x+x1
+    cy= y+y1
+    return cx , cy
+detect = []
+
+offset = 6 #Allowable error between pixel
+counter = 0
+    
+
+while True:
+    ret,frame1= cap.read()
+    if not ret:
+        break
+    grey = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(grey,(3,3),5)
+    # applying on each frame
+
+    img_sub = algo.apply(blur)
+    dilat = cv2.dilate(img_sub, np.ones((5,5)))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    dilatada = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
+    dilatada = cv2.morphologyEx(dilatada, cv2.MORPH_CLOSE, kernel)
+    counterShape,h = cv2.findContours(dilatada, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.line(frame1, (25, count_line_position), (1200, count_line_position),(255,127,0),3)
+
+    for (i,c) in enumerate(counterShape):
+        (x,y,w,h) = cv2.boundingRect(c)
+        validate_counter = (w>= min_width_rect) and (h>= min_hieght_rect)
+        if not validate_counter: 
+            continue
+
+        cv2.rectangle(frame1, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        cv2.putText(frame1, "VEHICLE" +str(counter), (x, y-20), cv2.FONT_HERSHEY_TRIPLEX,1, (255,244,0),2)
+
+
+        center = center_handle(x,y,w,h)
+        detect.append(center)
+        cv2.circle(frame1, center, 4, (0,0,255), -1)
+
+        for (x,y) in detect:
+            if  y<(count_line_position+offset) and y>(count_line_position-offset):
+                counter += 1
+                cv2.line(frame1, (25, count_line_position), (1200, count_line_position),(0,127,255),3)
+                detect.remove((x,y))
+
+                print("vehicle Counter:" + str(counter))
+
+
+    
+    cv2.putText(frame1, "VEHICLE COUNTER :" +str(counter), (450,70), cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),5)
+
+
+
+
+    cv2.imshow('Video Original',frame1)
+
+    if cv2.waitKey(1) == 13:
+        break
+
 cv2.destroyAllWindows()
 cap.release()
